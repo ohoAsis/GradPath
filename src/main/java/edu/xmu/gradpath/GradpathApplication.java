e08eed3a003fa8bd57d16db457e4cd4d1b7f7fc5
ohoasis <851691158@qq.com>
2026-01-29 12:33:39 +0800

    init: add core dependencies and openapi support

---
+++ src/main/java/edu/xmu/gradpath/GradpathApplication.java
@@ -0,0 +1,13 @@
+package edu.xmu.gradpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradpathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/xmu/gradpath/GradpathApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.xmu.gradpath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradpathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
